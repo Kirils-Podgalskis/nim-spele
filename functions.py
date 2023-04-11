64be75e83bab99907f09b7b5595563a717c32a9b
kirilspodgalskis <kirils.podgalskis@loadero.com>
2023-04-12 00:02:59 +0300

    FINISH

--- functions.py
+++ functions.py
@@ -6,19 +6,13 @@ def generate_children(node:Node, depth:int):
             pile = node.state[pile_idx]
             if pile > 0:
                 for stones in range(1,pile+1):
-                    if node.is_max_player():
-                        new_state = node.state.copy()
-                        new_state[pile_idx] -= stones
-                        new_node = Node(new_state, "Min")
-                        node.add_child(new_node)
-                        generate_children(new_node, depth-1)
-                    else:
-                        new_state = node.state.copy()
-                        new_state[pile_idx] -= stones
-                        new_node = Node(new_state, "Max")
-                        node.add_child(new_node)
-                        generate_children(new_node, depth-1)
-    else :
+                    new_state = node.state.copy()
+                    new_state[pile_idx] -= stones
+                    if node.is_max_player(): new_node = Node(new_state, "Min")
+                    else: new_node = Node(new_state, "Max")
+                    node.add_child(new_node)
+                    generate_children(new_node, depth-1)
+    else:
         return
 
 def minimax(node:Node, depth:int, is_max:bool):
@@ -44,59 +38,3 @@ def minimax(node:Node, depth:int, is_max:bool):
         node.move = best_child.state
         return best_value
 
-def get_human_move(piles:list):
-    valid_move = False
-    while not valid_move:
-        pile_idx = int(input("Enter the index of the pile you want to remove stones from (0, 1, or 2): "))
-        if pile_idx < 0 or pile_idx > 2:
-            print("Invalid pile index. Please enter a valid index.")
-            continue
-        if piles[pile_idx] == 0:
-            print("The selected pile is already empty. Please select another pile.")
-            continue
-        num_stones = int(input(f"Enter the number of stones you want to remove from pile {pile_idx}: "))
-        if num_stones < 1 or num_stones > piles[pile_idx]:
-            print("Invalid number of stones. Please enter a valid number.")
-            continue
-        valid_move = True
-    return [pile_idx, num_stones]
-
-def play_game():
-    piles = [2,3,2]  # Starting piles
-    player = "human"  # Human goes first
-
-    while True:
-        print(f"\nCurrent state: {piles}")
-        
-        if player == "human":
-            while True:
-                pile_idx = int(input("Enter pile index (1, 2, or 3): "))-1
-                if 0 <= pile_idx < 3 and piles[pile_idx] > 0:
-                    break
-                else:
-                    print("Invalid pile index or pile is empty, try again.")
-
-            while True:
-                stones = int(input("Enter number of stones to take: "))
-                if 1 <= stones <= piles[pile_idx]:
-                    break
-                else:
-                    print(f"Invalid number of stones, must be between 1 and {piles[pile_idx]}, try again.")
-
-            piles[pile_idx] -= stones
-            player = "computer"
-        else:
-            print("Computer is thinking...")
-            root_node = Node(piles, "Max")
-            generate_children(root_node, depth=2)
-            best_choise = minimax(root_node, depth=2, is_max=True)
-            print(f"Computer move: {root_node.state} -> {root_node.move}, its advantage: {best_choise}")
-            piles = root_node.move
-            player = "human"
-
-        if all(p == 0 for p in piles):
-            if player == "human":
-                print("Computer wins!")
-            else:
-                print("You win!")
-            break
--- main.py
+++ main.py
@@ -1,4 +1,110 @@
 from node import *
 from functions import *
+import tkinter as tk
+from tkinter import *
 
-play_game()
+window = Tk()
+window.title("Nim Game")
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window_width = screen_width*0.75
+window_height = screen_height*0.75
+window.resizable(width=False, height=False)
+
+title_label= tk.Label(master=window, text="Nim Game", font=("Arial", 30),background="blue")
+title_label.grid(row=0, column=0, columnspan=3,sticky='ew')
+
+rules_label = tk.Label(master=window, text="Rules: \n1. There are 3 piles of stones. \n2. You can take any amount of stones from any, but only one pile. \n3. The player who takes the last stone wins.", font=("Arial", 15), background="red")
+rules_label.grid(row=1, column=0, rowspan=2,sticky='ew')
+
+question_label = tk.Label(master=window, text="Who goes first?", font=("Arial", 15), background="green")
+question_label.grid(row=1,column=1,rowspan=2,sticky='ew')
+
+first_move = StringVar()
+Radiobutton(window, text="Human", variable=first_move, value="human").grid(row=1, column=2,sticky='ew')
+Radiobutton(window, text="Computer", variable=first_move, value="computer").grid(row=2, column=2,sticky='ew')
+
+for i in range(3):
+        question=f"How many stones in pile No.{i+1}?"
+        tk.Label(master=window, text=question).grid(row=3, column=i)
+
+first_input= Entry(window)
+first_input.grid(row=4, column=0,sticky='ew')
+
+second_input= Entry(window)
+second_input.grid(row=4, column=1,sticky='ew')
+
+third_input= Entry(window)
+third_input.grid(row=4, column=2,sticky='ew')
+
+def submit():
+    first_pile = int(first_input.get())
+    second_pile = int(second_input.get())
+    third_pile = int(third_input.get())
+
+    piles = [first_pile,second_pile,third_pile]
+    player = first_move.get()
+
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    play_game(piles, player)
+
+start_game_btn = tk.Button(master=window, text="Start Game", background="green", command=submit)
+start_game_btn.grid(row=5, column=1,sticky='ew', rowspan=3)
+
+def play_game(piles,player):  # Starting piles and who goes first
+    render(piles)
+    if player == "human":
+        pile_input= Entry(window)
+        pile_input.grid(row=2, column=1, columnspan=2,sticky='ew')
+
+        stones_input= Entry(window)
+        stones_input.grid(row=3, column=1, columnspan=2,sticky='ew')
+
+        def take():
+            pile_idx = int(pile_input.get())-1
+            amount = int(stones_input.get())
+            piles[pile_idx] -= amount
+            if all(p == 0 for p in piles):
+                for widget in window.winfo_children():
+                    widget.destroy()
+                you_won_label = tk.Label(master=window, text="You Won!", font=("Arial", 30),background="blue")
+                you_won_label.place(relx=0.5, rely=0.5, anchor=CENTER)
+                return
+            player = "computer"
+            play_game(piles, player)
+
+        take_btn = tk.Button(master=window, text="Take", background="green", command=take)
+        take_btn.grid(row=4, column=0,columnspan=3,sticky='ew')
+    else:
+        root_node = Node(piles, "Max")
+        generate_children(root_node, depth=2)
+        minimax(root_node, depth=2, is_max=True)
+        print(root_node)
+
+        piles = root_node.move
+        player = "human"
+        if all(p == 0 for p in piles):
+            for widget in window.winfo_children():
+                widget.destroy()
+            you_lost_label = tk.Label(master=window, text="You Lost!", font=("Arial", 30),background="blue")
+            you_lost_label.place(relx=0.5, rely=0.5, anchor=CENTER)
+            return
+        play_game(piles, player)
+
+def render(piles):
+    title_label= tk.Label(master=window, text="Nim Game", font=("Arial", 30),background="blue")
+    title_label.grid(row=0, column=0, columnspan=3, sticky='ew')
+
+    pick_label = tk.Label(master=window,width=17, text="Pick a pile", font=("Arial", 15), background="gray")
+    pick_label.grid(row=2, column=0, sticky='ew')
+
+    stones_label = tk.Label(master=window,width=17, text="Pick a number of stones", font=("Arial", 15), background="gray")
+    stones_label.grid(row=3, column=0, sticky='ew')
+
+    for i in range(3):
+        tk.Label(master=window, text=str(piles[i]), width=6,height=3, relief=RAISED, font=("Helvetica",36,"bold") ,background="black").grid(row=1, column=i,sticky='ew')
+    window.update()
+
+window.mainloop()
